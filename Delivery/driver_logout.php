980bbc883b32167b3074d88da853eb6086c50401
Ririyami <oliviaazcuna@gmail.com>
2025-05-21 22:31:37 +0800

    Delete Delivery directory

--- Delivery/driver_logout.php
+++
@@ -1,8 +0,0 @@
-<!-- filepath: c:\Users\Wrhinnon\OneDrive\Desktop\Xaampp\htdocs\bagdeluxe\Delivery\logout.php -->
-<?php
-session_start();
-session_unset();
-session_destroy();
-header('Location: driver_login.php');
-exit;
-?>
