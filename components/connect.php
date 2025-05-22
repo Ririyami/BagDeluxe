c604180250dfcf7c88a96c67754d9043913da616
Ririyami <oliviaazcuna@gmail.com>
2025-05-22 11:25:53 +0800

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -4,9 +4,9 @@
  $user_name = 'root';
  $user_password = '';
 
-$db_name = 'mysql:host=localhost;dbname=u866427573_bagdeluxe';
-$user_name = 'u866427573_bagdeluxe';
-$user_password = '@Qetu1357';
+//$db_name = 'mysql:host=localhost;dbname=u866427573_bagdeluxe';
+//$user_name = 'u866427573_bagdeluxe';
+//$user_password = '@Qetu1357';
 
 
 $conn = new PDO($db_name, $user_name, $user_password);
