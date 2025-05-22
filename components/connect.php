eff3f215fbf57b7c2d2ca3e193ca50eb3d64181d
mbhornido <angelohornido2@gmail.com>
2025-05-22 11:08:51 +0800

    home

--- components/connect.php
+++ components/connect.php
@@ -6,7 +6,7 @@
 
 $db_name = 'mysql:host=localhost;dbname=u866427573_bagdeluxe';
 $user_name = 'u866427573_bagdeluxe';
-$user_password = 'Qetu1357';
+$user_password = '@Qetu1357';
 
 
 $conn = new PDO($db_name, $user_name, $user_password);
