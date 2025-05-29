97e8206ff252acb0f470163d4e20d3e7b04d1702
mbhornido <angelohornido2@gmail.com>
2025-05-29 23:24:40 +0800

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -4,9 +4,17 @@
 //  $user_name = 'root';
 //  $user_password = '';
 
-$db_name = 'mysql:host=localhost;dbname=u866427573_bagdeluxe';
+$host = 'localhost';
+$db_name = 'u866427573_bagdeluxe';
 $user_name = 'u866427573_bagdeluxe';
 $user_password = '@Qetu1357';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db_name", $user_name, $user_password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 
 $conn = new PDO($db_name, $user_name, $user_password);
 
